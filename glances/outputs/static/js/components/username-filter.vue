20869b3b3c5e36858e8165ab79c5a30a85b7e9d9
doughty-liam <liamdoughty5@gmail.com>
2024-10-20 17:59:38 +0000

    Began work on 'filter menu' within username-filter component. Need to adjust formatting and add  to new form. Also need to add 'close' button of some kind to the filter menu popup. Testing required.'

--- glances/outputs/static/js/components/username-filter.vue
+++ glances/outputs/static/js/components/username-filter.vue
@@ -1,14 +1,36 @@
 <template>
     <div id="username-filter-container">
-        <input id="filter-form" type="text" placeholder="Enter a username to filter by ..." @input="$emit('filterApplied', $event.target.value)">
+        <button id="open-filter-menu" @click="openFilterMenu()">Filter</button>
+        <div id="filter-menu" v-show="filterMenuOpen">
+            <div>Username pattern:</div>
+            <input type="text">
+        </div>
+        <!-- <input id="filter-form" type="text" placeholder="Enter a username to filter by ..." @input="$emit('filterApplied', $event.target.value)"> -->
     </div>
 </template>
 
 <script>
 
     export default {
-        emits: ["filterApplied"]
-    }
+        emits: ["filterApplied"],
+
+        data() {
+            return {
+                filterMenuOpen: false
+            }
+        },
+
+        methods: {
+            openFilterMenu() {
+                if(this.filterMenuOpen == false) {
+                    this.filterMenuOpen = true;
+                } else {
+                    this.filterMenuOpen = false;
+                }
+                console.log(this.filterMenuOpen);
+            }
+        }
+    };
     
 </script>
 
@@ -20,6 +42,25 @@
 
     #filter-form {
         width: 27%;
+    }
+
+    #open-filter-menu {
+        height: 100%;
+        width: 80px;
+        font-family: "Lucida Sans Typewriter", "Lucida Console", Monaco, "Bitstream Vera Sans Mono", monospace;
+    }
+
+    #filter-menu {
+        background-color: #000000;
+        border-radius: 2px;
+        border-color: #FFFFFF;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        height: 200px;
+        width: 400px;
+        font-family: "Lucida Sans Typewriter", "Lucida Console", Monaco, "Bitstream Vera Sans Mono", monospace;
     }
 
 </style>
